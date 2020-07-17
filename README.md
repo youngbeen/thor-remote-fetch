893084eea9c1b980424798c5236b5d4bc10d4d60
xhyang2 <xhyang2@chinaums.com>
2020-07-17 11:56:45 +0800

    feat: first commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# thor-remote-fetch
+
+The plugin provides ability of fetching configuration from remote for [Thor](https://github.com/youngbeen/thor)
+
+## How to use
+
+Under a thor project root, run commands
+
+1. `npm i thor-remote-fetch --save-dev`
+2. Make sure the api path is set correctly in config file
+3. `thor-remote-fetch` and done!
---
+++ src/index.js
@@ -0,0 +1,45 @@
+#!/usr/bin/env node
+
+const axios = require('../node_modules/axios')
+const fs = require('fs')
+
+const fetchData = () => {
+  // 开始获取
+  let content = fs.readFileSync('src/models/SystemConfig.js', {
+    encoding: 'utf-8'
+  })
+  let url = content.match(/(?<=systemConfigRequestUrl: ').*(?=',)/)[0] || ''
+  if (!url) {
+    console.error('配置文档中未配置获取配置信息的api url！')
+    return
+  }
+  console.log(`开始获取... - ${url}`)
+  axios.get(url).then(data => {
+    // console.log(data.data)
+    if (data && data.status === 200) {
+      console.log('获得远程配置接口应答')
+      fixConfig(data.data.data)
+    }
+  }).catch(err => {
+    console.log(err)
+  })
+}
+
+const fixConfig = (data) => {
+  // data<js object>, content<js string>
+  if (!data) {
+    console.error('无远程业务数据！')
+    return
+  }
+  let remoteBizPages = data
+  let content = fs.readFileSync('src/models/SystemConfig.js', { encoding: 'utf-8' })
+  let newContent = content.replace(/(?<=remotePages:)(.|\r|\n)*(?=})/, remoteBizPages)
+  // console.log(newContent)
+
+  fs.writeFileSync('src/models/SystemConfig.js', newContent, {
+    encoding: 'utf-8'
+  })
+  console.log('写入远程业务数据完成')
+}
+
+fetchData()
